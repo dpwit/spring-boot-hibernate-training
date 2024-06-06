988aa80fa3318188d86f5bcfb85e4dccf6b0fc04
Darren Whatford <darrenwhatford@hastingsdirect.com>
2024-06-06 08:51:58 +0100

    Set up files for new Spring Boot Hibernate course

---
+++ setup/src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // simply print out Hello World! to ensure Java is running and jdk 17 is installed.
+        System.out.println("Hello World!");
+    } // enn main method
+} // end HelloWorld class
